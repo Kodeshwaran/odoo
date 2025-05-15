340c5df50cfa416430ba6ecfd1a3eba602738a8e
Kodeshwaran <132118579+Kodeshwaran@users.noreply.github.com>
2025-05-15 15:01:15 +0530

    __manifest__.py

--- odoo/farming/__manifest__.py
+++ odoo/farming/__manifest__.py
@@ -2,25 +2,15 @@
 {
     'name': 'Farming',
     'version': '15.1',
-    'category': 'Invoicing',
-    'author': 'Futurenet Technologies',
+    'category': 'Farming',
+    'author': 'Farming',
     'description': """
        """,
-    'depends': ['base','account','base_accounting_kit', 'account_standard_report'],
+    'depends': [],
     'installable': True,
     'auto_install': False,
-    'data' : [
-             'security/ir.model.access.csv',
-             'data/account_person_email_corn.xml',
-             'views/report_layout.xml',
-             'views/account_invoice_inherit.xml',
-             'views/account_payment_inherit.xml',
-             'wizard/account_invoice_wizard.xml',
-             'wizard/inovice_due_report.xml',
-              ],
-    'qweb': [
-            ],
-
+    'data' : [  ],
+   
 }
 
 
